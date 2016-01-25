65eb7a9ef7ba15ccd99dd8a04302147a61eb9aaf
David Smith <david@startingdreams.com>
2016-01-25 18:26:28 -0500

    Added google analytics tracking.

--- src/angular/module.js
+++ src/angular/module.js
@@ -7,6 +7,21 @@
         'sdPages',
         'sdTemplates',
         'ui.bootstrap'
-    ]);
+    ])
+        .run(function($rootScope, $location, $window, sdConfigService) {
+
+            sdConfigService.config.then(function(config) {
+
+                // Setup Google Analytics
+                if (config.data.analytics) {
+                    $window.ga('create', 'account', 'auto');
+                    $rootScope.$on('$stateChangeSuccess',function(event) {
+                        $window.ga('send', 'pageview', $location.path());
+                    });
+                }
+
+            });
+
+        });
 
 })();
