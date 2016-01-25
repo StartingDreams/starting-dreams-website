2203940d5cf9c9851df7a920458d75626c5860f3
David Smith <david@startingdreams.com>
2016-01-25 01:27:50 -0500

    Changes to content.

--- src/angular/pages/services/body.directive.js
+++ src/angular/pages/services/body.directive.js
@@ -8,9 +8,14 @@
             var controller = function(sdStateService, anchorSmoothScroll) {
                 var vm = this;
                 vm.state = sdStateService;
-                vm.gotoElement = function (id) {
-                    anchorSmoothScroll.scrollTo(id, 30, 51);
-                };
+                //vm.gotoElement = function (id) {
+                //    anchorSmoothScroll.scrollTo(id, 30, 51);
+                //};
+
+                vm.showSection = null;
+                vm.changeSection = function(section) {
+                    vm.showSection = section;
+                }
             };
 
             return {
