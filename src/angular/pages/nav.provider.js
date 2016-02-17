d25bed590944afcd924c6992820c3012c9be2801
David Smith <david@startingdreams.com>
2016-02-16 22:59:48 -0500

    Removed contact block and most pages.

--- src/angular/pages/nav.provider.js
+++ src/angular/pages/nav.provider.js
@@ -25,64 +25,6 @@
                     data: {
                         fullHeight: true
                     }
-                },
-                {
-                    icon: 'fa-cogs',
-                    label: 'Services',
-                    sref: 'services',
-                    url: '/services',
-                    navbar: true,
-                    sidebar: true,
-                    contact: true,
-                    home: true,
-                    views: {
-                        'body@': {
-                            template: '<sd-services-body></sd-services-body>'
-                        },
-                        'sidebar@': {
-                            template: '<sd-sidebar-nav></sd-sidebar-nav>'
-                        }
-                    },
-                    data: {}
-                },
-                {
-                    icon: 'fa-question-circle',
-                    label: 'About',
-                    sref: 'about',
-                    url: '/about',
-                    navbar: true,
-                    sidebar: true,
-                    contact: true,
-                    home: true,
-                    views: {
-                        'body@': {
-                            template: '<sd-about-body></sd-about-body>'
-                        },
-                        'sidebar@': {
-                            template: '<sd-sidebar-nav></sd-sidebar-nav>'
-                        }
-                    },
-                    data: {}
-
-                },
-                {
-                    icon: 'fa-phone',
-                    label: 'Contact',
-                    sref: 'contact',
-                    url: '/contact-us',
-                    navbar: true,
-                    sidebar: true,
-                    contact: true,
-                    home: true,
-                    views: {
-                        'body@': {
-                            template: '<sd-contact-body></sd-contact-body>'
-                        },
-                        'sidebar@': {
-                            template: '<sd-sidebar-nav></sd-sidebar-nav>'
-                        }
-                    },
-                    data: {}
                 }
             ];
 
